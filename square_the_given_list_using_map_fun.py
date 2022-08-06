25231e22e823d37c4cf8169a75debb6d37eb6d25
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2022-08-06 14:36:30 +0530

    Square_the_element_in_list_using_map_fun
    Python program to square the elements of a list using map() function.

---
+++ square_the_given_list_using_map_fun.py
@@ -0,0 +1,7 @@
+def square_num(no1):
+  return no1 * no1
+nums = list(int(i) for i in input("Enter the element of the List to Square it using map fuction : "))
+print("\nOriginal List: ",nums)
+result_Square = list(map(square_num, nums))
+print("\nSquare the elements of the said list using map():")
+print(list(result_Square))
