4d6f4e2af8ae4a112d16cde4341823add0f46b9b
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2022-08-06 14:38:50 +0530

    Update square_the_given_list_using_map_fun.py

--- square_the_given_list_using_map_fun.py
+++ square_the_given_list_using_map_fun.py
@@ -4,4 +4,4 @@ nums = list(int(i) for i in input("Enter the element of the List to Square it us
 print("\nOriginal List: ",nums)
 result_Square = list(map(square_num, nums))
 print("\nSquare the elements of the said list using map():")
-print(list(result_Square))
+print(result_Square)
